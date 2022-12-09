da078dfc2ec4d83de08c3b9bc5c625a9d8028bdd
Edvinas <edvinasalimas98@gmail.com>
2022-12-09 07:39:52 +0200

    SPORT-23 Add default Exercises and Categories when User registers

--- App/Workout/Repositories/UserRepository/UserRepository.cs
+++ App/Workout/Repositories/UserRepository/UserRepository.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using System.Security.Claims;
 using Microsoft.IdentityModel.Tokens;
 using AutoMapper;
@@ -53,6 +54,39 @@ namespace SaveApp.App.Workout.Repositories.UserRepository
             entity.PasswordSalt = passwordSalt;
 
             _context.User.Add(entity);
+            _context.SaveChanges();
+
+            List<ExerciseCategoryEntity> categoryEntities = DefaultListOfCategories();
+            categoryEntities.ForEach(o => o.User = entity);
+
+            _context.AddRange(categoryEntities);
+            _context.SaveChanges();
+
+            List<ExerciseEntity> absExercises = DefaultListOfAbsExercises();
+            absExercises.ForEach(o => { o.User = entity; o.ExerciseCategory = categoryEntities.Where(o => o.Name == "Abs").Single(); });
+            List<ExerciseEntity> backExercises = DefaultListOfBackExercises();
+            backExercises.ForEach(o => { o.User = entity; o.ExerciseCategory = categoryEntities.Where(o => o.Name == "Back").Single(); });
+            List<ExerciseEntity> bicepsExercises = DefaultListOfBicepExercises();
+            bicepsExercises.ForEach(o => { o.User = entity; o.ExerciseCategory = categoryEntities.Where(o => o.Name == "Biceps").Single(); });
+            List<ExerciseEntity> cardioExercises = DefaultListOfCardioExercises();
+            cardioExercises.ForEach(o => { o.User = entity; o.ExerciseCategory = categoryEntities.Where(o => o.Name == "Cardio").Single(); });
+            List<ExerciseEntity> chestExercises = DefaultListOfChestExercises();
+            chestExercises.ForEach(o => { o.User = entity; o.ExerciseCategory = categoryEntities.Where(o => o.Name == "Chest").Single(); });
+            List<ExerciseEntity> legsExercises = DefaultListOfLegsExercises();
+            legsExercises.ForEach(o => { o.User = entity; o.ExerciseCategory = categoryEntities.Where(o => o.Name == "Legs").Single(); });
+            List<ExerciseEntity> shouldersExercises = DefaultListOfShouldersExercises();
+            shouldersExercises.ForEach(o => { o.User = entity; o.ExerciseCategory = categoryEntities.Where(o => o.Name == "Shoulders").Single(); });
+            List<ExerciseEntity> tricepsExercises = DefaultListOfTricepsExercises();
+            tricepsExercises.ForEach(o => { o.User = entity; o.ExerciseCategory = categoryEntities.Where(o => o.Name == "Triceps").Single(); });
+
+            _context.AddRange(absExercises);
+            _context.AddRange(backExercises);
+            _context.AddRange(bicepsExercises);
+            _context.AddRange(cardioExercises);
+            _context.AddRange(chestExercises);
+            _context.AddRange(legsExercises);
+            _context.AddRange(shouldersExercises);
+            _context.AddRange(tricepsExercises);
             _context.SaveChanges();
 
             return _mapper.Map<User>(entity);
@@ -111,6 +145,105 @@ namespace SaveApp.App.Workout.Repositories.UserRepository
             SecurityToken token = tokenHandler.CreateToken(tokenDescriptor);
 
             return tokenHandler.WriteToken(token);
+        }
+
+        private static List<ExerciseCategoryEntity> DefaultListOfCategories() {
+            return new List<ExerciseCategoryEntity> {
+                new ExerciseCategoryEntity { Name = "Abs" },
+                new ExerciseCategoryEntity { Name = "Back" },
+                new ExerciseCategoryEntity { Name = "Biceps" },
+                new ExerciseCategoryEntity { Name = "Cardio" },
+                new ExerciseCategoryEntity { Name = "Chest" },
+                new ExerciseCategoryEntity { Name = "Legs" },
+                new ExerciseCategoryEntity { Name = "Shoulders" },
+                new ExerciseCategoryEntity { Name = "Triceps" }
+            };
+        }
+
+        private static List<ExerciseEntity> DefaultListOfAbsExercises() {
+            return new List<ExerciseEntity> {
+                new ExerciseEntity { Name = "Crunches", Note = "", ExerciseType = ExerciseType.STRENGTH_WEIGHT_REPS, IsSingleBodyPartExercise = false },
+                new ExerciseEntity { Name = "Leg Raises", Note = "", ExerciseType = ExerciseType.STRENGTH_WEIGHT_REPS, IsSingleBodyPartExercise = false }
+            };
+        }
+
+        private static List<ExerciseEntity> DefaultListOfBackExercises() {
+            return new List<ExerciseEntity> {
+                new ExerciseEntity { Name = "Assisted Chin Up", Note = "", ExerciseType = ExerciseType.STRENGTH_WEIGHT_REPS, IsSingleBodyPartExercise = false },
+                new ExerciseEntity { Name = "Assisted Pull Up", Note = "", ExerciseType = ExerciseType.STRENGTH_WEIGHT_REPS, IsSingleBodyPartExercise = false },
+                new ExerciseEntity { Name = "Barbell Row", Note = "", ExerciseType = ExerciseType.STRENGTH_WEIGHT_REPS, IsSingleBodyPartExercise = false },
+                new ExerciseEntity { Name = "Cable Row", Note = "", ExerciseType = ExerciseType.STRENGTH_WEIGHT_REPS, IsSingleBodyPartExercise = false },
+                new ExerciseEntity { Name = "Chin Up", Note = "", ExerciseType = ExerciseType.STRENGTH_WEIGHT_REPS, IsSingleBodyPartExercise = false },
+                new ExerciseEntity { Name = "Deadlift", Note = "", ExerciseType = ExerciseType.STRENGTH_WEIGHT_REPS, IsSingleBodyPartExercise = false },
+                new ExerciseEntity { Name = "Dumbbell Row", Note = "", ExerciseType = ExerciseType.STRENGTH_WEIGHT_REPS, IsSingleBodyPartExercise = false },
+                new ExerciseEntity { Name = "Hyperextensions", Note = "", ExerciseType = ExerciseType.STRENGTH_WEIGHT_REPS, IsSingleBodyPartExercise = false },
+                new ExerciseEntity { Name = "Pull Up", Note = "", ExerciseType = ExerciseType.STRENGTH_WEIGHT_REPS, IsSingleBodyPartExercise = false },
+                new ExerciseEntity { Name = "Pulldowns", Note = "", ExerciseType = ExerciseType.STRENGTH_WEIGHT_REPS, IsSingleBodyPartExercise = false },
+            };
+        }
+
+        private static List<ExerciseEntity> DefaultListOfBicepExercises() {
+            return new List<ExerciseEntity> {
+                new ExerciseEntity { Name = "Barbell Bicep Curl", Note = "", ExerciseType = ExerciseType.STRENGTH_WEIGHT_REPS, IsSingleBodyPartExercise = false },
+                new ExerciseEntity { Name = "Concentration Curl", Note = "", ExerciseType = ExerciseType.STRENGTH_WEIGHT_REPS, IsSingleBodyPartExercise = false },
+                new ExerciseEntity { Name = "Dumbbell Bicep Curl", Note = "", ExerciseType = ExerciseType.STRENGTH_WEIGHT_REPS, IsSingleBodyPartExercise = false },
+                new ExerciseEntity { Name = "Hammer Curl", Note = "", ExerciseType = ExerciseType.STRENGTH_WEIGHT_REPS, IsSingleBodyPartExercise = false }
+            };
+        }
+
+        private static List<ExerciseEntity> DefaultListOfCardioExercises() {
+            return new List<ExerciseEntity> {
+                new ExerciseEntity { Name = "Cycling", Note = "", ExerciseType = ExerciseType.STRENGTH_WEIGHT_REPS, IsSingleBodyPartExercise = false },
+                new ExerciseEntity { Name = "Eliptical Trainer", Note = "", ExerciseType = ExerciseType.STRENGTH_WEIGHT_REPS, IsSingleBodyPartExercise = false },
+                new ExerciseEntity { Name = "Rowing Machine", Note = "", ExerciseType = ExerciseType.STRENGTH_WEIGHT_REPS, IsSingleBodyPartExercise = false },
+                new ExerciseEntity { Name = "Running", Note = "", ExerciseType = ExerciseType.STRENGTH_WEIGHT_REPS, IsSingleBodyPartExercise = false },
+                new ExerciseEntity { Name = "Treadmill", Note = "", ExerciseType = ExerciseType.STRENGTH_WEIGHT_REPS, IsSingleBodyPartExercise = false },
+                new ExerciseEntity { Name = "Walking", Note = "", ExerciseType = ExerciseType.STRENGTH_WEIGHT_REPS, IsSingleBodyPartExercise = false }
+            };
+        }
+
+        private static List<ExerciseEntity> DefaultListOfChestExercises() {
+            return new List<ExerciseEntity> {
+                new ExerciseEntity { Name = "Bench Press", Note = "", ExerciseType = ExerciseType.STRENGTH_WEIGHT_REPS, IsSingleBodyPartExercise = false },
+                new ExerciseEntity { Name = "Cable Crossovers", Note = "", ExerciseType = ExerciseType.STRENGTH_WEIGHT_REPS, IsSingleBodyPartExercise = false },
+                new ExerciseEntity { Name = "Dumbbell Flies", Note = "", ExerciseType = ExerciseType.STRENGTH_WEIGHT_REPS, IsSingleBodyPartExercise = false },
+                new ExerciseEntity { Name = "Dumbbell Press", Note = "", ExerciseType = ExerciseType.STRENGTH_WEIGHT_REPS, IsSingleBodyPartExercise = false },
+                new ExerciseEntity { Name = "Incline Bench Press", Note = "", ExerciseType = ExerciseType.STRENGTH_WEIGHT_REPS, IsSingleBodyPartExercise = false },
+                new ExerciseEntity { Name = "Incline Dumbbell Press", Note = "", ExerciseType = ExerciseType.STRENGTH_WEIGHT_REPS, IsSingleBodyPartExercise = false }
+            };
+        }
+
+        private static List<ExerciseEntity> DefaultListOfLegsExercises() {
+            return new List<ExerciseEntity> {
+                new ExerciseEntity { Name = "Calf Raises", Note = "", ExerciseType = ExerciseType.STRENGTH_WEIGHT_REPS, IsSingleBodyPartExercise = false },
+                new ExerciseEntity { Name = "Front Squat", Note = "", ExerciseType = ExerciseType.STRENGTH_WEIGHT_REPS, IsSingleBodyPartExercise = false },
+                new ExerciseEntity { Name = "Leg Curls", Note = "", ExerciseType = ExerciseType.STRENGTH_WEIGHT_REPS, IsSingleBodyPartExercise = false },
+                new ExerciseEntity { Name = "Leg Extensions", Note = "", ExerciseType = ExerciseType.STRENGTH_WEIGHT_REPS, IsSingleBodyPartExercise = false },
+                new ExerciseEntity { Name = "Leg Press", Note = "", ExerciseType = ExerciseType.STRENGTH_WEIGHT_REPS, IsSingleBodyPartExercise = false },
+                new ExerciseEntity { Name = "Lunges", Note = "", ExerciseType = ExerciseType.STRENGTH_WEIGHT_REPS, IsSingleBodyPartExercise = false },
+                new ExerciseEntity { Name = "Seated Calf Raises", Note = "", ExerciseType = ExerciseType.STRENGTH_WEIGHT_REPS, IsSingleBodyPartExercise = false },
+                new ExerciseEntity { Name = "Squat", Note = "", ExerciseType = ExerciseType.STRENGTH_WEIGHT_REPS, IsSingleBodyPartExercise = false },
+                new ExerciseEntity { Name = "Straight Leg Deadlifts", Note = "", ExerciseType = ExerciseType.STRENGTH_WEIGHT_REPS, IsSingleBodyPartExercise = false }
+            };
+        }
+
+        private static List<ExerciseEntity> DefaultListOfShouldersExercises() {
+            return new List<ExerciseEntity> {
+                new ExerciseEntity { Name = "Dumbbell Lateral Raises", Note = "", ExerciseType = ExerciseType.STRENGTH_WEIGHT_REPS, IsSingleBodyPartExercise = false },
+                new ExerciseEntity { Name = "Military Press", Note = "", ExerciseType = ExerciseType.STRENGTH_WEIGHT_REPS, IsSingleBodyPartExercise = false },
+                new ExerciseEntity { Name = "Shoulder Dumbbell Press", Note = "", ExerciseType = ExerciseType.STRENGTH_WEIGHT_REPS, IsSingleBodyPartExercise = false },
+                new ExerciseEntity { Name = "Upright Rows", Note = "", ExerciseType = ExerciseType.STRENGTH_WEIGHT_REPS, IsSingleBodyPartExercise = false }
+            };
+        }
+
+        private static List<ExerciseEntity> DefaultListOfTricepsExercises() {
+            return new List<ExerciseEntity> {
+                new ExerciseEntity { Name = "Assisted Dips", Note = "", ExerciseType = ExerciseType.STRENGTH_WEIGHT_REPS, IsSingleBodyPartExercise = false },
+                new ExerciseEntity { Name = "Close Grip Bench Press", Note = "", ExerciseType = ExerciseType.STRENGTH_WEIGHT_REPS, IsSingleBodyPartExercise = false },
+                new ExerciseEntity { Name = "Dips", Note = "", ExerciseType = ExerciseType.STRENGTH_WEIGHT_REPS, IsSingleBodyPartExercise = false },
+                new ExerciseEntity { Name = "Pushdowns", Note = "", ExerciseType = ExerciseType.STRENGTH_WEIGHT_REPS, IsSingleBodyPartExercise = false },
+                new ExerciseEntity { Name = "Triceps Extensions", Note = "", ExerciseType = ExerciseType.STRENGTH_WEIGHT_REPS, IsSingleBodyPartExercise = false }
+            };
         }
     }
 }
